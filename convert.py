692870aced31ed7711649ca6b30853bdfb636b19
Saad Masood <sm22ae@herts.ac.uk>
2024-01-29 15:51:56 +0000

    changes

--- convert.py
+++ convert.py
@@ -1,7 +1,13 @@
+# Make sure to install Pillow first:
+# pip install Pillow
+
+# Replace input.jpg with the name of your JPG file and output.bmp with the desired BMP file name.
+
+
 from PIL import Image
 
 # Open the JPG file
-img = Image.open('PATH')
+img = Image.open('input.jpg')
 
 # Save as BMP
-img.save('image.bmp')
+img.save('output.bmp')
