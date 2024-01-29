b9b61745bccdb6252e185ee76f6c7efdf855dfc4
Saad Masood <sm22ae@herts.ac.uk>
2024-01-29 15:49:00 +0000

    changes

--- convert.py
+++ convert.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
 # Open the JPG file
-img = Image.open('C:/Users/saadm/Downloads/chicken_house_slides_nov_2023_5.jpg')
+img = Image.open('PATH')
 
 # Save as BMP
-img.save('chicken_house_slides_nov_2023_5.bmp')
+img.save('image.bmp')
