ee766ab36a4260cf3024f5adc9bf4ca1fa28e968
Saad Masood <sm22ae@herts.ac.uk>
2024-01-29 15:47:28 +0000

    first commit

---
+++ convert.py
@@ -0,0 +1,7 @@
+from PIL import Image
+
+# Open the JPG file
+img = Image.open('C:/Users/saadm/Downloads/chicken_house_slides_nov_2023_5.jpg')
+
+# Save as BMP
+img.save('chicken_house_slides_nov_2023_5.bmp')
